3fcc48b6c3e476a5d5a2ec35f64ca8e821629a70
Ephraim Jude <Ephraimjude1@gmail.com>
2024-02-01 14:45:31 +0100

    vercel

--- src/components/navbar/link/links.jsx
+++ src/components/navbar/link/links.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import styles from './links.module.css';
-import NavLink from "./navLink/navLink";
+import NavLink from "./NavLink/navLInk";
 import Image from 'next/image';
 
 const links = [
